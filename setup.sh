bab5ba002a07d81df51f38aac5469415bd19c320
muyiGin <muyi.gin@sjtu.edu.cn>
2024-09-24 22:29:29 +0800

    vim_test

--- setup.sh
+++ setup.sh
@@ -1,18 +1,9 @@
 #! /bin/bash
 
-if command -v git &> /dev/null; then
-	echo "Git is already installed."
-else
-	echo "Git is not installed, Installing..."
-	sudo apt install -y git
-fi
-cd /
-git clone git@github.com:muyiGin/linux-environment-setup
-
 if command -v vim &> /dev/null; then
 	echo "Vim is already installed."
 else
 	echo "Vim is not installed, Installing..."
-	sudo apt install -y vim
+	apt install -y vim
 fi
 
