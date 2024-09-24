922521df19802376f60dcd8cb6e5b3b4e29f9c4d
muyiGin <muyi.gin@sjtu.edu.cn>
2024-09-24 22:44:47 +0800

    add pwndbg

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
 #! /bin/bash
 
 if command -v vim &> /dev/null; then
-	echo "Vim is already installed."
+	echo "Vim has already installed."
 else
-	echo "Vim is not installed, Installing..."
+	echo "Vim has not installed, Installing..."
 	apt install -y vim
 fi
+if command -v gdb &> /dev/null; then
+	echo "Gdb has already installed."
+else
+	echo "Gdb has not installed, Installing..."
+	apt install -y gdb
+fi
 
+cd /
+if !(find / -iname "pwndbg" 2>/dev/null | grep -q .); then
+	echo "Pwndbg has not installed, Installing..."
+	git clone https://github.com/pwndbg/pwndbg
+fi
+cd pwndbg
+./setup.sh
