321495ed758ee47556706981d1b47c841c4fbdc8
muyiGin <muyi.gin@sjtu.edu.cn>
2024-09-27 00:46:08 +0800

    add ->

--- setup.sh
+++ setup.sh
@@ -4,40 +4,40 @@ Working_Directory=$(cd "$(dirname "$0")";pwd)
 ####################Check & Download######################
 # Curl
 if command -v curl &> /dev/null; then
-	echo "Curl has already installed."
+	echo "->Curl has already installed."
 else
-	echo "Curl has not installed, Installing..."
+	echo "->Curl has not installed, Installing..."
 	apt install -y curl
 fi
 # Vim
 if command -v vim &> /dev/null; then
-	echo "Vim has already installed."
+	echo "->Vim has already installed."
 else
-	echo "Vim has not installed, Installing..."
+	echo "->Vim has not installed, Installing..."
 	apt install -y vim
 fi
 # Vim-plug
 if !(find ~ -iname "plug.vim" 2>/dev/null | grep -q .); then
-	echo "Vim-plug has not installed, Installing..."
+	echo "->Vim-plug has not installed, Installing..."
 	curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-echo "Vim-plug has already installed."
+echo "->Vim-plug has already installed."
 # Gdb
 if command -v gdb &> /dev/null; then
-	echo "Gdb has already installed."
+	echo "->Gdb has already installed."
 else
-	echo "Gdb has not installed, Installing..."
+	echo "->Gdb has not installed, Installing..."
 	apt install -y gdb
 fi
 # gdb-Pwndbg
 if !(find ~ -iname "pwndbg" 2>/dev/null | grep -q .); then
-	echo "Pwndbg has not installed, Installing..."
+	echo "->Pwndbg has not installed, Installing..."
 	git clone https://github.com/pwndbg/pwndbg
 	cd pwndbg
 	./setup.sh
 fi
-echo "Pwndbg has already installed."
+echo "->Pwndbg has already installed."
 ####################Copy & Paste######################
 cp $Working_Directory/.tmux.conf ~/
 cp $Working_Directory/.vimrc ~/
